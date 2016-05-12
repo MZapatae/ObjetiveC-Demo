af5506903927f953be1b4cc302868158692870bb
Miguel A. Zapata <miguel.zapatae@gmail.com>
2016-05-12 15:53:07 -0300

    Initial Commit

---
+++ Demo ObjetiveC/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Demo ObjetiveC
+//
+//  Created by Miguel Zapata on 12-05-16.
+//  Copyright © 2016 Miguel A. Zapata. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
